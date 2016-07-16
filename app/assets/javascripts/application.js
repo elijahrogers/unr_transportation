36ec3f38bcdc9f2f4340b956c91b0b3d33e5cb17
Elijah Rogers <elijahrogers@charter.net>
2016-07-15 17:38:37 -0700

    Add basic UI and Google Maps functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,98 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require underscore
+//= require gmaps/google
+
+var mapStyle = [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#46bcec"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+];
+var handler = Gmaps.build('Google');
+handler.buildMap({
+    internal: {id: 'map'},
+    provider: {
+      zoom:      15,
+      center:    new google.maps.LatLng(39.5439642, -119.8171444),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles:    mapStyle
+    }
+  },
+  function(){ }
+);
