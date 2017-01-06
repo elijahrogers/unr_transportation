bf10edef271cc61b02b164159b74c584b44931cc
Elijah Rogers <elijahrogers@charter.net>
2017-01-05 19:37:05 -0800

    Refactored application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,32 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include UNRParkingZonesLogic
   include ParkingPassInfo
 
   layout 'application'
-  
+
   def main
     @pass = ''
     find_available_zones
   end
 
   def update
-    @pass = params[:pass]
-    @number = params[:number]
-    @time = (params[:time]).gsub(':', '').to_i
+    update_params
     parking_pass_info
     find_available_zones
-    render json:{pass: @pass, number: @number, time: @time, zones: @zones, info: @info}
+    render json: {
+      pass: @pass,
+      number: @number,
+      time: @time,
+      zones: @zones,
+      info: @info
+    }
+  end
+
+  def update_params
+    @pass = params[:pass]
+    @number = params[:number]
+    @time = params[:time].delete(':').to_i
   end
 end
