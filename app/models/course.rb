744dfad9afee1ea450b177e6c496c113331cc8c4
Elijah Rogers <elijahrogers@charter.net>
2017-01-17 19:44:09 -0800

    Refactored courses partial in accordance with the Law of Demeter

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
   belongs_to :user
   belongs_to :building
+  delegate :name, to: :building, prefix: true
 
   validates :name, presence: true,
                    length: { within: 5...50 }
